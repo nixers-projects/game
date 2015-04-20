759fdf540af5b4266fe0a893e462634e4325359a
Ville Hakulinen <ville.hakulinen@gmail.com>
2015-04-20 15:35:50 +0300

    Nuke everything!

--- image.h
+++
@@ -1,6 +0,0 @@
-#ifndef IMAGE
-#define IMAGE
-
-bool initImg();
-SDL_Surface* loadImage(SDL_Surface* screenSurface,char path[]);
-#endif
--- music.h
+++
@@ -1,9 +0,0 @@
-#ifndef MUSIC
-#define MUSIC
-
-bool initAudio();
-Mix_Music * loadMusic(char path[]);
-void playMusic();
-void cleanUp();
-
-#endif
